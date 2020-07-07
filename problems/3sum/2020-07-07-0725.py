94610989282273818df0c272111b3780d7223f72
Jongsun Suh <jongsun.suh@icloud.com>
2020-07-07 07:25:17 +0000

    Add another accepted version of 3Sum in Python

---
+++ problems/3sum/2020-07-07-0725.py
@@ -0,0 +1,36 @@
+# 3Sum
+# https://leetcode.com/problems/3sum/
+# Accepted 2020-07-07 07:25 UTC · Python · 1272 ms · 17.3 MB
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        n = len(nums)
+        if n >= 3:
+            nums.sort()
+            for i in range(n - 2):
+                if i > 0 and nums[i] == nums[i - 1]:
+                    continue
+                if nums[i] + nums[n - 2] + nums[n - 1] < 0:
+                    continue
+                if nums[i] + nums[i + 1] + nums[i + 2] > 0:
+                    break
+            
+                a = nums[i]
+                j = i + 1
+                k = n - 1
+                while j < k:
+                    b, c = nums[j], nums[k]
+                    if b + c == -a: 
+                        results.append([a, b, c])
+                        while j < k and nums[j] == nums[j + 1]:
+                            j += 1
+                        j += 1
+                        while k > j and nums[k] == nums[k - 1]:
+                            k -= 1
+                        k -= 1
+                    elif b + c > -a:
+                        k -= 1
+                    elif b + c < -a:
+                        j += 1
+        return results
