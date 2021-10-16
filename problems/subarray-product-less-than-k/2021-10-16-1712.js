823ccd0a6d4451e67f66b30ed96b1b0513c2c076
Jongsun Suh <jongsun.suh@icloud.com>
2021-10-16 17:12:01 +0000

    Add another accepted version of Subarray Product Less Than K in JavaScript

---
+++ problems/subarray-product-less-than-k/2021-10-16-1712.js
@@ -0,0 +1,29 @@
+// Subarray Product Less Than K
+// https://leetcode.com/problems/subarray-product-less-than-k/
+// Accepted 2021-10-16 17:12 UTC · JavaScript · 1564 ms · 44.8 MB
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function(nums, k) {
+    let [product, counter] = [1, 0];
+    for (let [left, right] = [0, 0]; ;) {
+        product *= nums[right];
+        if (product < k) {
+            counter++;
+            if (right < nums.length - 1) right++;
+            else {
+                product = 1;
+                right = ++left;
+            }
+        }
+        else {
+            product = 1;
+            if (left < nums.length - 1) right = ++left;
+            else break;
+        }
+    }
+    return counter;
+};
