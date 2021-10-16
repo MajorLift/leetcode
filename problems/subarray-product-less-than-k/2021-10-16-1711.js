69564e922ce75c81f6f826cfedd66c3de26e5431
Jongsun Suh <jongsun.suh@icloud.com>
2021-10-16 17:11:18 +0000

    Solve Subarray Product Less Than K in JavaScript

---
+++ problems/subarray-product-less-than-k/2021-10-16-1711.js
@@ -0,0 +1,29 @@
+// Subarray Product Less Than K
+// https://leetcode.com/problems/subarray-product-less-than-k/
+// Accepted 2021-10-16 17:11 UTC · JavaScript · 1520 ms · 45 MB
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function(nums, k) {
+    let [product, counter] = [1, 0];
+    for (let [left, right] = [0, 0]; left <= right && right < nums.length;) {
+        product *= nums[right];
+        if (product < k) {
+            counter++;
+            if (right < nums.length - 1) right++;
+            else {
+                product = 1;
+                right = ++left;
+            }
+        }
+        else {
+            product = 1;
+            if (left < nums.length - 1) right = ++left;
+            else break;
+        }
+    }
+    return counter;
+};
