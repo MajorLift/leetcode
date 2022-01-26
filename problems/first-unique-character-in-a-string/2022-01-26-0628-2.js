76afda7cd48e4e04bc8edda25d3f67d1dd415117
Jongsun Suh <jongsun.suh@icloud.com>
2022-01-26 06:28:58 +0000

    Add another accepted version of First Unique Character in a String in JavaScript

---
+++ problems/first-unique-character-in-a-string/2022-01-26-0628-2.js
@@ -0,0 +1,22 @@
+// First Unique Character in a String
+// https://leetcode.com/problems/first-unique-character-in-a-string/
+// Accepted 2022-01-26 06:28 UTC · JavaScript · 225 ms · 47.6 MB
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const map = s.split('').reduce((acc, curr, i) => {
+        if (acc.has(curr)) {
+            const [count, idx] = acc.get(curr);
+            acc.set(curr, [count + 1, i]);
+        }
+        else acc.set(curr, [1, i]);
+        return acc;
+    }, new Map());
+    for (const [key, value] of map) {
+        if (value[0] === 1) return value[1];
+    }
+    return -1;
+};
