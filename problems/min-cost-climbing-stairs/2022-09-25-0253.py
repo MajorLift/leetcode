609b2c1b15227d387d4459a2e4fa10602c1088ba
Jongsun Suh <jongsun.suh@icloud.com>
2022-09-25 02:53:36 +0000

    Solve Min Cost Climbing Stairs in Python

---
+++ problems/min-cost-climbing-stairs/2022-09-25-0253.py
@@ -0,0 +1,11 @@
+# Min Cost Climbing Stairs
+# https://leetcode.com/problems/min-cost-climbing-stairs/
+# Accepted 2022-09-25 02:53 UTC · Python · 103 ms · 14.1 MB
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0 for _ in range(n + 2)]
+        for i in range(n - 1, -1, -1):
+            dp[i] = cost[i] + min(dp[i + 1], dp[i + 2])
+        return min(dp[0], dp[1])
