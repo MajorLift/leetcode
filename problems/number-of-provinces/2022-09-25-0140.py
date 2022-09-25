a33264be1fae76642f392e235a45cf039bf4ab9e
Jongsun Suh <jongsun.suh@icloud.com>
2022-09-25 01:40:55 +0000

    Solve Number of Provinces in Python

---
+++ problems/number-of-provinces/2022-09-25-0140.py
@@ -0,0 +1,42 @@
+# Number of Provinces
+# https://leetcode.com/problems/number-of-provinces/
+# Accepted 2022-09-25 01:40 UTC · Python · 450 ms · 14.4 MB
+
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+        self.count = size
+        
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+        
+    def union(self, x, y):
+        rootX, rootY = self.find(x), self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+            self.count -= 1
+        
+    def getCount(self):
+        return self.count
+    
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        if not isConnected or len(isConnected) == 0:
+            return 0
+        n = len(isConnected)
+        uf = UnionFind(n)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if isConnected[i][j] == 1:
+                    uf.union(i, j)
+        return uf.getCount()
