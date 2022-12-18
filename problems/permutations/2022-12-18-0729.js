16d156e8efed56759a20427c0a44267bbb16eb5f
Jongsun Suh <jongsun.suh@icloud.com>
2022-12-18 07:29:00 +0000

    Add another accepted version of Permutations in JavaScript

---
+++ problems/permutations/2022-12-18-0729.js
@@ -0,0 +1,27 @@
+// Permutations
+// https://leetcode.com/problems/permutations/
+// Accepted 2022-12-18 07:29 UTC · JavaScript · 90 ms · 44.9 MB
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+  const output = []
+  ;(function backtrack(path = [], used = new Array(nums.length).fill(false)) {
+    if (path.length === nums.length) {
+      output.push(path.slice())
+      return
+    }
+    for (let i = 0; i < nums.length; ++i) {
+      if (!used[i]) {
+        used[i] = true
+        path.push(nums[i])
+        backtrack(path, used)
+        path.pop()
+        used[i] = false
+      }
+    }
+  })()
+  return output
+};
