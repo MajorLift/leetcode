e96ad593e86640b7f79346907c91e7df15812a88
Jongsun Suh <jongsun.suh@icloud.com>
2022-12-21 12:55:08 +0000

    Add another accepted version of Word Search in Python

---
+++ problems/word-search/2022-12-21-1255.py
@@ -0,0 +1,30 @@
+# Word Search
+# https://leetcode.com/problems/word-search/
+# Accepted 2022-12-21 12:55 UTC · Python · 3804 ms · 14.1 MB
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+        k = len(word)
+
+        word_chars, board_chars = set([*word]), set([e for row in board for e in row])
+        if m * n < k or not board_chars.issuperset(word_chars):
+            return False
+
+        visited = set()
+        def backtrack(curr, idx):
+            x, y = curr
+            if not (0 <= x < m and 0 <= y < n) or board[x][y] != word[idx] or (x, y) in visited:
+                return False
+            if idx == k - 1:
+                return True
+
+            visited.add(curr)
+            for i, j in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
+                if backtrack((i, j), idx + 1):
+                    return True
+            visited.remove(curr)
+            return False
+
+        return any([backtrack((start_row, start_col), 0) for start_row, start_col in \
+            [(i, j) for i in range(m) for j in range(n) if board[i][j] == word[0]]])
