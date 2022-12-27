14f06245c61a162dbeb8134ee87a2204f3a4e48e
Jongsun Suh <jongsun.suh@icloud.com>
2022-12-27 08:48:05 +0000

    Add another accepted version of N-Queens II in Python

---
+++ problems/n-queens-ii/2022-12-27-0848.py
@@ -0,0 +1,31 @@
+# N-Queens II
+# https://leetcode.com/problems/n-queens-ii/
+# Accepted 2022-12-27 08:48 UTC · Python · 33 ms · 13.9 MB
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        cols, diags, antidiags = set(), set(), set()
+
+        output = 0
+        def backtrack(row):
+            nonlocal output
+            if row == n:
+                output += 1
+                return
+            
+            for col in range(n):
+                if col in cols or row - col in diags or row + col in antidiags:
+                    continue
+
+                cols.add(col)
+                diags.add(row - col)
+                antidiags.add(row + col)
+
+                backtrack(row + 1)
+
+                cols.remove(col)
+                diags.remove(row - col)
+                antidiags.remove(row + col)
+
+        backtrack(0)
+        return output
