6e5c2ac3f0ff2dd74f4866cf3101288f9792423f
Jongsun Suh <jongsun.suh@icloud.com>
2023-01-03 03:59:25 +0000

    Add another accepted version of Pacific Atlantic Water Flow in Python

---
+++ problems/pacific-atlantic-water-flow/2023-01-03-0359.py
@@ -0,0 +1,18 @@
+# Pacific Atlantic Water Flow
+# https://leetcode.com/problems/pacific-atlantic-water-flow/
+# Accepted 2023-01-03 03:59 UTC · Python · 286 ms · 15.7 MB
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        m, n = len(heights), len(heights[0])
+
+        def dfs(r, c, visited: set[tuple[int, int]]) -> None:
+            visited.add((r, c))
+            for i, j in ((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)):
+                if 0 <= i < m and 0 <= j < n and (i, j) not in visited and heights[i][j] >= heights[r][c]:
+                    dfs(i, j, visited)
+
+        pacific, atlantic = set(), set()
+        [dfs(x, y, pacific) for x, y in [*[(i, 0) for i in range(m)], *[(0, j + 1) for j in range(n - 1)]]]
+        [dfs(x, y, atlantic) for x, y in [*[(i, n - 1) for i in range(m)], *[(m - 1, j) for j in range(n - 1)]]]
+        return pacific & atlantic
