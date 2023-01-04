0105c22c09382d38a7b211b6212b5cea9cf5a875
Jongsun Suh <jongsun.suh@icloud.com>
2023-01-04 00:19:28 +0000

    Add another accepted version of Pacific Atlantic Water Flow in JavaScript

---
+++ problems/pacific-atlantic-water-flow/2023-01-04-0019.js
@@ -0,0 +1,30 @@
+// Pacific Atlantic Water Flow
+// https://leetcode.com/problems/pacific-atlantic-water-flow/
+// Accepted 2023-01-04 00:19 UTC · JavaScript · 145 ms · 49.8 MB
+
+/**
+ * @param {number[][]} heights
+ * @return {number[][]}
+ */
+var pacificAtlantic = function(heights) {
+    const [m, n] = [heights.length, heights[0].length]
+    function dfs(r, c, visited) {
+        visited.add([r, c].join(','))
+        for (const [i, j] of [[r - 1, c], [r + 1, c], [r, c - 1], [r, c + 1]]) {
+            if (i >= 0 && i < m && j >= 0 && j < n && !visited.has([i, j].join(',')) && heights[i][j] >= heights[r][c]) {
+                dfs(i, j, visited)
+            }
+        }
+        return visited
+    }
+    const [pacific, atlantic] = [new Set(), new Set()]
+    for (let i = 0; i < m; ++i) {
+        dfs(i, 0, pacific)
+        dfs(i, n - 1, atlantic)
+    }
+    for (let j = 0; j < n - 1; ++j) {
+        dfs(0, j + 1, pacific)
+        dfs(m - 1, j, atlantic)
+    }
+    return [...pacific].filter((e) => atlantic.has(e)).map((e) => e.split(','))
+}
