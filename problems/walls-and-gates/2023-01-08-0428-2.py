51ac3a7d382aa7bf705c13cc2303d592fd26db01
Jongsun Suh <jongsun.suh@icloud.com>
2023-01-08 04:28:36 +0000

    Add another accepted version of Walls and Gates in Python

---
+++ problems/walls-and-gates/2023-01-08-0428-2.py
@@ -0,0 +1,17 @@
+# Walls and Gates
+# https://leetcode.com/problems/walls-and-gates/
+# Accepted 2023-01-08 04:28 UTC · Python · 253 ms · 16.8 MB
+
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        m, n, INF = len(rooms), len(rooms[0]), 2147483647
+        queue = collections.deque([(i, j) for i, j in itertools.product(range(m), range(n)) if rooms[i][j] == 0])
+        while queue:
+            r, c = queue.popleft()
+            for i, j in ((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)):
+                if 0 <= i < m and 0 <= j < n and rooms[r][c] + 1 < rooms[i][j]:
+                    rooms[i][j] = rooms[r][c] + 1
+                    queue.append((i, j))
