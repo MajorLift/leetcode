bdd27d8ff8a9741779ae6239508389d3e83670a7
Jongsun Suh <jongsun.suh@icloud.com>
2023-01-18 21:09:51 +0000

    Solve Word Ladder in JavaScript

---
+++ problems/word-ladder/2023-01-18-2109.js
@@ -0,0 +1,46 @@
+// Word Ladder
+// https://leetcode.com/problems/word-ladder/
+// Accepted 2023-01-18 21:09 UTC · JavaScript · 164 ms · 56.6 MB
+
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+    const words = new Set(wordList)
+    const visited = new Set()
+    if (!words.has(endWord)) return 0
+
+    const k = beginWord.length
+    const adj = wordList.reduce((acc, curr) => {
+        for (let i = 0; i < k; ++i) {
+            const pattern = curr.slice(0, i) + "*" + curr.slice(i + 1)
+            acc[pattern] = (acc[pattern] ?? []).concat(curr)
+        }
+        return acc
+    }, {})
+    
+    const queue = [beginWord]
+    let dist = 0
+    while (queue.length) {
+        dist++
+        const next_queue = []
+        while (queue.length) {
+            const word = queue.shift()
+            if (word === endWord) return dist
+            for (let i = 0; i < k; ++i) {
+                const pattern = word.slice(0, i) + "*" + word.slice(i + 1)
+                for (const s of (adj[pattern] ?? [])) {
+                    if (!visited.has(s) && words.has(s)) {
+                        visited.add(s)
+                        next_queue.push(s)
+                    }
+                }
+            }
+        }
+        queue.push(...next_queue)
+    }
+    return 0
+};
