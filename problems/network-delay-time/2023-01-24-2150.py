5a003190d44b172ff78ba57dec487abbd0e759ea
Jongsun Suh <jongsun.suh@icloud.com>
2023-01-24 21:50:40 +0000

    Add another accepted version of Network Delay Time in Python

---
+++ problems/network-delay-time/2023-01-24-2150.py
@@ -0,0 +1,24 @@
+# Network Delay Time
+# https://leetcode.com/problems/network-delay-time/
+# Accepted 2023-01-24 21:50 UTC · Python · 682 ms · 15.9 MB
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj = [[-1 for _ in range(n + 1)] for _ in range(n + 1)]
+        for u, v, w in times:
+            adj[u][v] = w
+        
+        mst = set()
+        dist = [-math.inf] + [0 if i == k else +math.inf for i in range(1, n + 1)]
+        pq = [(dist[k], k)]
+        while pq:
+            dist_u, u = heappop(pq)
+            mst.add(u)
+            for v, w in enumerate(adj[u]):
+                if w < 0:
+                    continue
+                if dist_u + w < dist[v] and v not in mst:
+                    dist[v] = dist_u + w
+                    heappush(pq, (dist[v], v))
+
+        return max(dist) if all(x < +math.inf for x in dist) else -1
