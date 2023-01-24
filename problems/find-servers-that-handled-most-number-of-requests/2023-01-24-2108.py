5bb1d983d94ee1bb19427695c511ffdcbfb32627
Jongsun Suh <jongsun.suh@icloud.com>
2023-01-24 21:08:35 +0000

    Add another accepted version of Find Servers That Handled Most Number of Requests in Python

---
+++ problems/find-servers-that-handled-most-number-of-requests/2023-01-24-2108.py
@@ -0,0 +1,23 @@
+# Find Servers That Handled Most Number of Requests
+# https://leetcode.com/problems/find-servers-that-handled-most-number-of-requests/
+# Accepted 2023-01-24 21:08 UTC · Python · 5251 ms · 34.6 MB
+
+class Solution:
+    def busiestServers(self, k: int, arrival: List[int], load: List[int]) -> List[int]:
+        cnt = [0 for _ in range(k)]
+        busy, available = [], [_id for _id in range(k)]
+        for i, (start, duration) in enumerate(zip(arrival, load)):
+            while busy and start >= busy[0][0]:
+                server_end, server_id = heappop(busy)
+                insort(available, server_id)
+
+            if not available:
+                continue
+            idx = bisect_left(available, i % k)
+            assigned_id = available[idx] if idx < len(available) else available[0]
+            available.remove(assigned_id)            
+
+            heappush(busy, (start + duration, assigned_id))
+            cnt[assigned_id] += 1
+        max_count = max(cnt)
+        return [i for i, e in enumerate(cnt) if e == max_count]
