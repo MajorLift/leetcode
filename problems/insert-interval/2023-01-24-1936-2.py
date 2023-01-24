1d7c076d92b377c4b3770e22e1c6486918f362aa
Jongsun Suh <jongsun.suh@icloud.com>
2023-01-24 19:36:41 +0000

    Add another accepted version of Insert Interval in Python

---
+++ problems/insert-interval/2023-01-24-1936-2.py
@@ -0,0 +1,15 @@
+# Insert Interval
+# https://leetcode.com/problems/insert-interval/
+# Accepted 2023-01-24 19:36 UTC · Python · 87 ms · 17.3 MB
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals = sorted(intervals + [newInterval], key=lambda x: x[0])
+        output = []
+        for start, end in intervals:
+            if not output or start > output[-1][1]:
+                output.append([start, end])
+            elif start <= output[-1][1] <= end:
+                prev_start, prev_end = output.pop()
+                output.append([prev_start, end])
+        return output
