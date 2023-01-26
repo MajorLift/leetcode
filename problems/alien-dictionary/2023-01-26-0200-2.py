030a99dc029e9c36080f1c458ce9c2ab0cab2dae
Jongsun Suh <jongsun.suh@icloud.com>
2023-01-26 02:00:36 +0000

    Add another accepted version of Alien Dictionary in Python

---
+++ problems/alien-dictionary/2023-01-26-0200-2.py
@@ -0,0 +1,37 @@
+# Alien Dictionary
+# https://leetcode.com/problems/alien-dictionary/
+# Accepted 2023-01-26 02:00 UTC · Python · 33 ms · 13.9 MB
+
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        if not words:
+            return ""
+        if len(words) == 1:
+            return "".join(set(words[0]))
+
+        adj = defaultdict(set)
+        indegrees = defaultdict(int)
+        for l, r in zip(words, words[1:]):
+            for char in set([*l] + [*r]):
+                indegrees[char] += 0
+            for u, v in zip(l, r):
+                if u != v:
+                    if v not in adj[u]:
+                        adj[u].add(v)
+                        indegrees[v] += 1
+                    break
+            else:
+                if len(l) > len(r):
+                    return ""
+        
+        output = []
+        queue = deque([char for char in indegrees.keys() if indegrees[char] == 0])
+        while queue:
+            curr = queue.popleft()
+            output.append(curr)
+            for v in adj[curr]:
+                indegrees[v] -= 1
+                if indegrees[v] == 0:
+                    queue.append(v)
+
+        return "".join(output) if len(output) == len(indegrees.keys()) else ""
