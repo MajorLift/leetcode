12a5c6d3da844c03efa5fb26be9edb3b56522e8b
Jongsun Suh <jongsun.suh@icloud.com>
2023-01-28 06:37:39 +0000

    Solve Maximum Number of Events That Can Be Attended II in Python

---
+++ problems/maximum-number-of-events-that-can-be-attended-ii/2023-01-28-0637.py
@@ -0,0 +1,17 @@
+# Maximum Number of Events That Can Be Attended II
+# https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended-ii/
+# Accepted 2023-01-28 06:37 UTC · Python · 953 ms · 200.1 MB
+
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        n = len(events)
+        events.sort(key=lambda x: x[0])
+
+        @cache
+        def dp(idx = 0, quota = k):
+            if quota == 0 or idx >= n: return 0
+            start, end, val = events[idx]
+            next_idx = bisect_right(events, end, lo=idx, key=lambda x: x[0])
+            return max(dp(idx + 1, quota), val + dp(next_idx, quota - 1))
+
+        return dp(0, k)
