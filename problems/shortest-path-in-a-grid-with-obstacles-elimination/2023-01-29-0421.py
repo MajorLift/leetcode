45c82bd3c9a1f9983227767895b5f1b6a2dc7029
Jongsun Suh <jongsun.suh@icloud.com>
2023-01-29 04:21:42 +0000

    Add another accepted version of Shortest Path in a Grid with Obstacles Elimination in Python

---
+++ problems/shortest-path-in-a-grid-with-obstacles-elimination/2023-01-29-0421.py
@@ -0,0 +1,34 @@
+# Shortest Path in a Grid with Obstacles Elimination
+# https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/
+# Accepted 2023-01-29 04:21 UTC · Python · 58 ms · 14 MB
+
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+        source, target = (0, 0), (m - 1, n - 1)
+
+        dist = [[(+math.inf, +math.inf) for _ in range(n)] for _ in range(m)]
+        dist[0][0] = (0, 0)
+        pq = [(sum(target), 0, 0, source)]
+        while pq:
+            heuristic, steps, removals, (r, c) = heappop(pq)
+            if (r, c) == target:
+                break
+            if k - removals >= heuristic - steps:
+                return heuristic
+
+            for i, j in (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1):
+                if not (0 <= i < m and 0 <= j < n):
+                    continue
+
+                min_steps_next, min_removals_next = dist[i][j]
+                steps_next, removals_next = steps + 1, removals + grid[i][j]
+
+                if steps_next < min_steps_next or removals_next < min_removals_next:
+                    dist[i][j] = (steps_next, removals_next)
+                    if removals_next <= k:
+                        heuristic_next = steps_next + sum(target) - sum((i, j))
+                        heappush(pq, (heuristic_next, steps_next, removals_next, (i, j)))
+
+        min_steps_to_target = dist[-1][-1][0]
+        return min_steps_to_target if min_steps_to_target < +math.inf else -1
