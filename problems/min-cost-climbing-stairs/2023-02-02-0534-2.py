35f4665c3ab8c976d3680f8b2158d3ffebf196bd
Jongsun Suh <jongsun.suh@icloud.com>
2023-02-02 05:34:40 +0000

    Add another accepted version of Min Cost Climbing Stairs in Python

---
+++ problems/min-cost-climbing-stairs/2023-02-02-0534-2.py
@@ -0,0 +1,10 @@
+# Min Cost Climbing Stairs
+# https://leetcode.com/problems/min-cost-climbing-stairs/
+# Accepted 2023-02-02 05:34 UTC · Python · 63 ms · 13.9 MB
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+      return min(functools.reduce(
+        lambda acc, curr: (min(acc[0], acc[1]) + curr, acc[0]), 
+        cost, 
+        (0, 0)))
