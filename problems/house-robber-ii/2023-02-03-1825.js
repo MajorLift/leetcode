b810cc8d6cac1876b724c0ddf3aa2f45bb1e8d40
Jongsun Suh <jongsun.suh@icloud.com>
2023-02-03 18:25:48 +0000

    Add another accepted version of House Robber II in JavaScript

---
+++ problems/house-robber-ii/2023-02-03-1825.js
@@ -0,0 +1,19 @@
+// House Robber II
+// https://leetcode.com/problems/house-robber-ii/
+// Accepted 2023-02-03 18:25 UTC · JavaScript · 62 ms · 42.1 MB
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if (nums.length <= 3) return Math.max(...nums)
+    const candidates = [nums.slice(0, nums.length - 1), nums.slice(1)]
+    const memo = Array.from({ length: 2 }, () => new Array(nums.length - 1).fill(null))
+    function dp(c, i) {
+        if (i < 0) return 0
+        if (memo[c][i] !== null) return memo[c][i]
+        return memo[c][i] = Math.max(candidates[c][i] + dp(c, i - 2), dp(c, i - 1))
+    }
+    return Math.max(dp(0, nums.length - 2), dp(1, nums.length - 2))
+};
