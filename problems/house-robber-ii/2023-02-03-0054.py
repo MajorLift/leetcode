a6092b20883bf0b612d0b306f9b3450e4f885981
Jongsun Suh <jongsun.suh@icloud.com>
2023-02-03 00:54:34 +0000

    Add another accepted version of House Robber II in Python

---
+++ problems/house-robber-ii/2023-02-03-0054.py
@@ -0,0 +1,16 @@
+# House Robber II
+# https://leetcode.com/problems/house-robber-ii/
+# Accepted 2023-02-03 00:54 UTC · Python · 32 ms · 14.1 MB
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 3:
+            return max(nums)
+        candidates = nums[:-1], nums[1:]
+        @cache
+        def dp(c, i = len(nums) - 2):
+            arr = candidates[c]
+            if i < 0:
+                return 0
+            return max(arr[i] + dp(c, i - 2), dp(c, i - 1))
+        return max(dp(0), dp(1))
