7db4b301245ed24969e361bfd58ffa5158992e00
Jongsun Suh <jongsun.suh@icloud.com>
2023-02-04 03:56:05 +0000

    Add another accepted version of House Robber in Python

---
+++ problems/house-robber/2023-02-04-0356.py
@@ -0,0 +1,12 @@
+# House Robber
+# https://leetcode.com/problems/house-robber/
+# Accepted 2023-02-04 03:56 UTC · Python · 32 ms · 13.8 MB
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return nums[0]
+        memo = [0] * len(nums)
+        for i in range(len(nums)):
+            memo[i] = max(nums[i] + memo[i - 2], memo[i - 1])
+        return memo[-1]
