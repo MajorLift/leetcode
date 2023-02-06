74ff2c05f3bde8eb268cfa2293f4b14985e848f9
Jongsun Suh <jongsun.suh@icloud.com>
2023-02-06 23:24:54 +0000

    Add another accepted version of House Robber II in Python

---
+++ problems/house-robber-ii/2023-02-06-2324.py
@@ -0,0 +1,13 @@
+# House Robber II
+# https://leetcode.com/problems/house-robber-ii/
+# Accepted 2023-02-06 23:24 UTC · Python · 31 ms · 13.9 MB
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 3: return max(nums)
+        memo = [[0 for _ in range(n - 1)] for _ in range(2)]
+        for c in range(2):
+            for i in range(n - 1):
+                memo[c][i] = max(nums[(i + c) % n] + memo[c][i - 2], memo[c][i - 1])
+        return max(memo[0][n - 2], memo[1][n - 2])
