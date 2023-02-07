45eafec10a22a32cd7d2a9545579896e9793a2f4
Jongsun Suh <jongsun.suh@icloud.com>
2023-02-07 19:30:53 +0000

    Add another accepted version of Longest Palindromic Substring in Python

---
+++ problems/longest-palindromic-substring/2023-02-07-1930.py
@@ -0,0 +1,18 @@
+# Longest Palindromic Substring
+# https://leetcode.com/problems/longest-palindromic-substring/
+# Accepted 2023-02-07 19:30 UTC · Python · 462 ms · 13.9 MB
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        def expand(l, r):
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return (l + 1, r - 1)
+
+        max_idx = 0, 0
+        for i in range(n):
+            max_idx = max(max_idx, expand(i, i + 1), expand(i - 1, i + 1), key=lambda x: x[1] - x[0])
+        l, r = max_idx
+        return s[l:r + 1]
