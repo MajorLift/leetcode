e162027befa48d0dd26f1571bd4dbb51444b14ee
Jongsun Suh <jongsun.suh@icloud.com>
2023-02-15 17:36:26 +0000

    Solve Word Break II in Python

---
+++ problems/word-break-ii/2023-02-15-1736.py
@@ -0,0 +1,16 @@
+# Word Break II
+# https://leetcode.com/problems/word-break-ii/
+# Accepted 2023-02-15 17:36 UTC · Python · 34 ms · 13.8 MB
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        n, wordSet = len(s), set(wordDict)
+        memo = [[] for _ in range(n + 1)]
+        for r in range(1, n + 1):
+            for l in range(r + 1):
+                if s[l:r] in wordSet:
+                    if l == 0:
+                        memo[r].append(s[l:r])
+                    else:
+                        memo[r].extend([" ".join([fragment, s[l:r]]) for fragment in memo[l]])
+        return memo[-1]
