4bf5a659b9a4b1fef7b9f727f56e6188ab361da9
Jongsun Suh <jongsun.suh@icloud.com>
2023-02-28 08:14:57 +0000

    Add another accepted version of Pow(x, n) in Python

---
+++ problems/powx-n/2023-02-28-0814-2.py
@@ -0,0 +1,14 @@
+# Pow(x, n)
+# https://leetcode.com/problems/powx-n/
+# Accepted 2023-02-28 08:14 UTC · Python · 28 ms · 14 MB
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def power(x, n):
+            if n == 0: return 1.0
+            sqrt = power(x, n // 2)
+            if n % 2 == 0:
+                return sqrt * sqrt
+            return sqrt * sqrt * x
+
+        return power(x, n) if n >= 0 else power(1 / x, -n)
