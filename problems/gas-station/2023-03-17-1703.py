beaa743b86ba59ca49967e28bb5362499a7f6d99
Jongsun Suh <jongsun.suh@icloud.com>
2023-03-17 17:03:18 +0000

    Solve Gas Station in Python

---
+++ problems/gas-station/2023-03-17-1703.py
@@ -0,0 +1,17 @@
+# Gas Station
+# https://leetcode.com/problems/gas-station/
+# Accepted 2023-03-17 17:03 UTC · Python · 1104 ms · 25.2 MB
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        budget = list(zip(gas, cost))
+        total = tank = start = 0
+        for i in range(n):
+            g, c = budget[i]
+            total += g - c
+            tank += g - c
+            if tank < 0:
+                start = i + 1
+                tank = 0
+        return start if total >= 0 else -1
