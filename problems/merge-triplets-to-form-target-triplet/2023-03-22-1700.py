8f8c8fe585b88e7ad4e72821e82d7183e78adece
Jongsun Suh <jongsun.suh@icloud.com>
2023-03-22 17:00:50 +0000

    Add another accepted version of Merge Triplets to Form Target Triplet in Python

---
+++ problems/merge-triplets-to-form-target-triplet/2023-03-22-1700.py
@@ -0,0 +1,11 @@
+# Merge Triplets to Form Target Triplet
+# https://leetcode.com/problems/merge-triplets-to-form-target-triplet/
+# Accepted 2023-03-22 17:00 UTC · Python · 2254 ms · 59.2 MB
+
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        x = y = z = 0
+        for a, b, c in triplets:
+            if all(u <= v for u, v in zip((a, b, c), target)):
+                x, y, z = max(a, x), max(b, y), max(c, z)
+        return [x, y, z] == target
