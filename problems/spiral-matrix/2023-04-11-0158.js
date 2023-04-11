ad56744f437b62f80f9389cdcf28b2293c3ef4d0
Jongsun Suh <jongsun.suh@icloud.com>
2023-04-11 01:58:11 +0000

    Solve Spiral Matrix in JavaScript

---
+++ problems/spiral-matrix/2023-04-11-0158.js
@@ -0,0 +1,31 @@
+// Spiral Matrix
+// https://leetcode.com/problems/spiral-matrix/
+// Accepted 2023-04-11 01:58 UTC · JavaScript · 69 ms · 41.6 MB
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+   let [nr, nc] = [matrix.length, matrix[0].length]
+   const output = []
+   while (nr > 1 || nc > 1) {
+      output.push(...(matrix.shift() ?? []))
+      nr--
+
+      for (let i = 0; i < nr; ++i) {
+         if (matrix[i].length) output.push(matrix[i].pop())
+      }
+      nc--
+
+      output.push(...(matrix.pop()?.reverse() ?? []))
+      nr--
+
+      for (let i = nr - 1; i >= 0; --i) {
+         if (matrix[i].length) output.push(matrix[i].shift())
+      }
+      nc--
+   }
+   if (matrix[0]?.length) output.push(...matrix[0])
+   return output
+};
