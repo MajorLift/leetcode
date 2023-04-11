7010e7a5441530318bacdc87e26a92359825025c
Jongsun Suh <jongsun.suh@icloud.com>
2023-04-11 04:29:25 +0000

    Add another accepted version of Spiral Matrix in Python

---
+++ problems/spiral-matrix/2023-04-11-0429.py
@@ -0,0 +1,16 @@
+# Spiral Matrix
+# https://leetcode.com/problems/spiral-matrix/
+# Accepted 2023-04-11 04:29 UTC · Python · 30 ms · 13.7 MB
+
+class Solution:
+   def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+      return matrix and [*matrix.pop(0)] + self.spiralOrder(self.rotate_ccw(matrix))
+
+   def rotate_ccw(self, matrix: List[List[int]]):
+      return matrix and self.transpose(self.reflect(matrix))
+
+   def reflect(self, matrix: List[List[int]]):
+      return [list(reversed(row)) for row in matrix]
+
+   def transpose(self, matrix: List[List[int]]):
+      return [[row[j] for row in matrix] for j in range(len(matrix[0]))]
