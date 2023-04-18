37f22b55cb7f4d4c109a2a2cdbcd9ee7a24fc78d
Jongsun Suh <jongsun.suh@icloud.com>
2023-04-18 04:05:53 +0000

    Add another accepted version of Happy Number in JavaScript

---
+++ problems/happy-number/2023-04-18-0405.js
@@ -0,0 +1,20 @@
+// Happy Number
+// https://leetcode.com/problems/happy-number/
+// Accepted 2023-04-18 04:05 UTC · JavaScript · 61 ms · 45.5 MB
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+function isHappy(n) {
+    let [slow, fast] = [sumOfSquares(n), sumOfSquares(sumOfSquares(n))]
+    while (true) {
+        if (slow === 1) return true
+        if (slow === fast) return false
+        ;[slow, fast] = [sumOfSquares(slow), sumOfSquares(sumOfSquares(fast))]
+    }
+}
+function sumOfSquares(n) {
+    const [last, rest] = [n % 10, Math.floor(n / 10)]
+    return last ** 2 + (n === 0 ? 0 : sumOfSquares(rest))
+}
