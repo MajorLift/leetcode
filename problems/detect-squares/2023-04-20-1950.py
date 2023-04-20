0f710351b6c24e30f03fa72265f8124209239eb6
Jongsun Suh <jongsun.suh@icloud.com>
2023-04-20 19:50:43 +0000

    Add another accepted version of Detect Squares in Python

---
+++ problems/detect-squares/2023-04-20-1950.py
@@ -0,0 +1,30 @@
+# Detect Squares
+# https://leetcode.com/problems/detect-squares/
+# Accepted 2023-04-20 19:50 UTC · Python · 391 ms · 16.2 MB
+
+class DetectSquares:
+
+    def __init__(self):
+        self.points = defaultdict(int)
+        self.points_x, self.points_y = defaultdict(list), defaultdict(list)
+
+    def add(self, point: List[int]) -> None:
+        x, y = point
+        self.points[(x, y)] += 1
+        self.points_x[x].append(y)
+        self.points_y[y].append(x)
+
+    def count(self, point: List[int]) -> int:
+        x0, y0 = point
+        xdist, ydist = defaultdict(set), defaultdict(set)
+        for x in self.points_y[y0]:
+            xdist[abs(x - x0)].add(x)
+        for y in self.points_x[x0]:
+            ydist[abs(y - y0)].add(y)
+        
+        return sum(
+            self.points[(x, y)] 
+            * self.points[(x, y0)] 
+            * self.points[(x0, y)]
+            for edge in xdist.keys() & ydist.keys() - {0}
+            for x, y in product(xdist[edge], ydist[edge]))
