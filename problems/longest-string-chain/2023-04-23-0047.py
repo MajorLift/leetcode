ed19be3e13ee7ec3955a929fbe382efcc85641a7
Jongsun Suh <jongsun.suh@icloud.com>
2023-04-23 00:47:51 +0000

    Add another accepted version of Longest String Chain in Python

---
+++ problems/longest-string-chain/2023-04-23-0047.py
@@ -0,0 +1,38 @@
+# Longest String Chain
+# https://leetcode.com/problems/longest-string-chain/
+# Accepted 2023-04-23 00:47 UTC · Python · 702 ms · 15.9 MB
+
+class Solution:
+    def longestStrChain(self, words) -> int:
+        length_map = defaultdict(set)
+        for word in words:
+            length_map[len(word)].add(word)
+        
+        adj = defaultdict(list)
+        indegree = {w: 0 for w in words}
+        for i in length_map.keys():
+            if i + 1 not in length_map:
+                continue
+            for shorter, longer in product(length_map[i], length_map[i + 1]):
+                if self.is_predecessor(shorter, longer):
+                    adj[shorter].append(longer)
+                    indegree[longer] += 1
+
+        @cache
+        def dfs(node):
+            return 1 + max([dfs(child) for child in adj[node]] or [0])
+        return max([dfs(k) for k,v in indegree.items() if v == 0])
+            
+    def is_predecessor(self, shorter, longer):
+        if len(shorter) + 1 != len(longer):
+            return False
+        skips = 0
+        i = j = 0
+        while j < len(longer):
+            if i < len(shorter) and shorter[i] == longer[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+                skips += 1
+        return skips == 1
