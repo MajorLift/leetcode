1a0f891815d63cd2b61e8eacba217d07c38d07e9
Jongsun Suh <jongsun.suh@icloud.com>
2023-04-30 02:33:28 +0000

    Solve Largest Color Value in a Directed Graph in Python

---
+++ problems/largest-color-value-in-a-directed-graph/2023-04-30-0233.py
@@ -0,0 +1,36 @@
+# Largest Color Value in a Directed Graph
+# https://leetcode.com/problems/largest-color-value-in-a-directed-graph/
+# Accepted 2023-04-30 02:33 UTC · Python · 3011 ms · 202.2 MB
+
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        n, m = map(len, (colors, edges))
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+        
+        ans = -1
+        visited, cycle, cnt = set(), set(), [[0] * 26 for _ in range(n)]
+        def dfs(node):
+            nonlocal ans
+            if node in cycle:
+                return True
+            if node in visited:
+                return False
+            visited.add(node)
+            cycle.add(node)
+            for v in adj[node]:
+                if dfs(v):
+                    return True
+                for color in range(26):
+                    cnt[node][color] = max(cnt[node][color], cnt[v][color])
+            cycle.remove(node)
+            cnt[node][ord(colors[node]) - ord("a")] += 1
+            ans = max(ans, cnt[node][ord(colors[node]) - ord("a")])
+            return False
+        
+        for i in range(n):
+            if i not in visited:
+                if dfs(i):
+                    return -1
+        return ans
