af7926e62d57216e3528ef1e4939f9d22ba6fd52
Jongsun Suh <jongsun.suh@icloud.com>
2023-05-01 19:06:22 +0000

    Solve Check if Word Can Be Placed In Crossword in Python

---
+++ problems/check-if-word-can-be-placed-in-crossword/2023-05-01-1906.py
@@ -0,0 +1,14 @@
+# Check if Word Can Be Placed In Crossword
+# https://leetcode.com/problems/check-if-word-can-be-placed-in-crossword/
+# Accepted 2023-05-01 19:06 UTC · Python · 1787 ms · 37.7 MB
+
+class Solution:
+    def placeWordInCrossword(self, board: List[List[str]], word: str) -> bool:
+        m, n, k = map(len, (board, board[0], word))
+        EMPTY, BLOCKED = ' ', '#'
+        for row in board + list(zip(*board)):
+            for s in ''.join(row).split('#'):
+                for w in (word, word[::-1]):
+                    if len(s) == k and all(s[i] == w[i] or s[i] == EMPTY for i in range(k)):
+                        return True
+        return False
