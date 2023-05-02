8387f9bb13fc70033e49e193ab8395d7ed68e40e
Jongsun Suh <jongsun.suh@icloud.com>
2023-05-02 16:56:30 +0000

    Add another accepted version of Height of Binary Tree After Subtree Removal Queries in Python

---
+++ problems/height-of-binary-tree-after-subtree-removal-queries/2023-05-02-1656.py
@@ -0,0 +1,38 @@
+# Height of Binary Tree After Subtree Removal Queries
+# https://leetcode.com/problems/height-of-binary-tree-after-subtree-removal-queries/
+# Accepted 2023-05-02 16:56 UTC · Python · 1104 ms · 88.8 MB
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
+        depths, heightsPQ = defaultdict(int), defaultdict(list)
+        
+        def dfs(node, depth):
+            if not node: return -1
+            depths[node.val] = depth
+            height = max(
+                dfs(node.left, depth + 1),
+                dfs(node.right, depth + 1)
+            ) + 1
+            heappush(heightsPQ[depth], (-height, node.val))
+            return height
+        dfs(root, 0)
+
+        answer = []
+        for query in queries:
+            depth = depths[query]
+            if len(heightsPQ[depth]) == 1:
+                answer.append(depth - 1)
+            elif heightsPQ[depth][0][1] == query:
+                tmp = heappop(heightsPQ[depth])
+                answer.append(-heightsPQ[depth][0][0] + depth)
+                heappush(heightsPQ[depth], tmp)
+            else:
+                answer.append(-heightsPQ[depth][0][0] + depth)
+
+        return answer
