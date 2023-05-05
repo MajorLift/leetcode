b952af7154527c18171dd13980de9447782b8805
Jongsun Suh <jongsun.suh@icloud.com>
2023-05-05 04:15:30 +0000

    Add another accepted version of Minimum Cost of a Path With Special Roads in Python

---
+++ problems/minimum-cost-of-a-path-with-special-roads/2023-05-05-0415.py
@@ -0,0 +1,30 @@
+# Minimum Cost of a Path With Special Roads
+# https://leetcode.com/problems/minimum-cost-of-a-path-with-special-roads/
+# Accepted 2023-05-05 04:15 UTC · Python · 2550 ms · 24.2 MB
+
+class Solution:
+    def minimumCost(self, start: List[int], target: List[int], specialRoads: List[List[int]]) -> int:
+        (xs, ys), (xt, yt) = start, target
+        adj = defaultdict(lambda: defaultdict(lambda: +inf))
+        nodes = set(map(tuple, (start, target)))
+        for x1, y1, x2, y2, w in specialRoads:
+            adj[(x1, y1)][(x2, y2)] = min(adj[(x1, y1)][(x2, y2)], w)
+            nodes.add((x1, y1))
+            nodes.add((x2, y2))
+        
+        dist = defaultdict(lambda: +inf)
+        dist[(xs, ys)] = 0
+        pq = [(0, (xs, ys))]
+        while pq:
+            dist_u, u = heappop(pq)
+            if u == target:
+                break
+            for v in nodes:
+                w = min(adj[u][v], self.getCost(u, v))
+                if dist_u + w < dist[v]:
+                    dist[v] = dist_u + w
+                    heappush(pq, (dist[v], v))
+        return dist[(xt, yt)]
+        
+    def getCost(self, a, b):
+        return abs(a[0] - b[0]) + abs(a[1] - b[1])
