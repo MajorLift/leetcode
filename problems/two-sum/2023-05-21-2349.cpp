eeb32137b939beeada54d402d8a4ba4a09ac3565
Jongsun Suh <jongsun.suh@icloud.com>
2023-05-21 23:49:45 +0000

    Add another accepted version of Two Sum in C++

---
+++ problems/two-sum/2023-05-21-2349.cpp
@@ -0,0 +1,17 @@
+// Two Sum
+// https://leetcode.com/problems/two-sum/
+// Accepted 2023-05-21 23:49 UTC · C++ · 12 ms · 10.9 MB
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (mp.find(target - nums[i]) != mp.end()) {
+                return {mp[target - nums[i]], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
