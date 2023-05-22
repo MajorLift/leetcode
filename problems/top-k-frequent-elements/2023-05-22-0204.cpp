f49a311523583d796a6f68ff871c3e403d9832f0
Jongsun Suh <jongsun.suh@icloud.com>
2023-05-22 02:04:07 +0000

    Add another accepted version of Top K Frequent Elements in C++

---
+++ problems/top-k-frequent-elements/2023-05-22-0204.cpp
@@ -0,0 +1,23 @@
+// Top K Frequent Elements
+// https://leetcode.com/problems/top-k-frequent-elements/
+// Accepted 2023-05-22 02:04 UTC · C++ · 15 ms · 13.6 MB
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> cnt;
+        for (const int& num : nums) {
+            ++cnt[num];
+        }
+        vector<pair<int, int>> p;
+        for (auto it = cnt.begin(); it != cnt.end(); ++it) {
+            p.emplace_back(make_pair(-(it->second), it->first));
+        }
+        nth_element(p.begin(), p.begin() + k - 1, p.end());
+        vector<int> output;
+        for (int i = 0; i < k; ++i) {
+            output.emplace_back(p[i].second);
+        }
+        return output;
+    }
+};
