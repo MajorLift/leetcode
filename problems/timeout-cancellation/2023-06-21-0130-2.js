37f889b7bfed9b1135dac367923b4b3a6853c9b9
Jongsun Suh <jongsun.suh@icloud.com>
2023-06-21 01:30:52 +0000

    Add another accepted version of Timeout Cancellation in JavaScript

---
+++ problems/timeout-cancellation/2023-06-21-0130-2.js
@@ -0,0 +1,34 @@
+// Timeout Cancellation
+// https://leetcode.com/problems/timeout-cancellation/
+// Accepted 2023-06-21 01:30 UTC · JavaScript · 71 ms · 41.9 MB
+
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    const handle = setTimeout(() => {
+        fn(...args)
+    }, t)
+    return () => clearTimeout(handle)
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const log = (...argsArr) => {
+ *      result.push(fn(...argsArr))
+ *  }
+ *       
+ *  const cancel = cancellable(fn, args, t);
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, cancelT)
+ */
