23bbb5657fe0959e7b7742f7745c1945a8691451
Jongsun Suh <jongsun.suh@icloud.com>
2023-09-04 22:02:58 +0000

    Add another accepted version of Sum of Two Integers in Python

---
+++ problems/sum-of-two-integers/2023-09-04-2202.py
@@ -0,0 +1,24 @@
+# Sum of Two Integers
+# https://leetcode.com/problems/sum-of-two-integers/
+# Accepted 2023-09-04 22:02 UTC · Python · 33 ms · 16.4 MB
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = (1 << 33) - 1
+
+        def full_adder(x: int, y: int, carry_in: int) -> tuple[int, int]:
+            sum_, carry_out = x ^ y ^ carry_in, (x & y) ^ (carry_in & (x ^ y))
+            return sum_, carry_out
+
+        def carry_save_adder(a: int, b: int) -> int:
+            output = sum_ = carry = i = 0
+            while a or b or carry:
+                sum_, carry = full_adder(a & 1, b & 1, carry)
+                output ^= sum_ << i
+                a >>= 1
+                b >>= 1
+                i += 1
+            return output
+
+        res = carry_save_adder(a & mask, b & mask) & mask
+        return res if not res & (1 << 31) else res ^ ~mask
