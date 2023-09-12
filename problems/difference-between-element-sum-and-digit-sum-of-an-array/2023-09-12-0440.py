5390256cc3dd55f97345b64abf8d9e84e3c8d412
Jongsun Suh <jongsun.suh@icloud.com>
2023-09-12 04:40:02 +0000

    Add another accepted version of Difference Between Element Sum and Digit Sum of an Array in Python

---
+++ problems/difference-between-element-sum-and-digit-sum-of-an-array/2023-09-12-0440.py
@@ -0,0 +1,15 @@
+# Difference Between Element Sum and Digit Sum of an Array
+# https://leetcode.com/problems/difference-between-element-sum-and-digit-sum-of-an-array/
+# Accepted 2023-09-12 04:40 UTC · Python · 192 ms · 16.6 MB
+
+class Solution:
+    def differenceOfSum(self, nums: List[int]) -> int:
+        def digit_iter(nums: List[int]):
+            while nums:
+                curr = nums.pop()
+                pos = 10 ** int(math.log10(curr))
+                yield curr // pos
+                curr -= (curr // pos) * pos
+                if curr:
+                    nums.append(curr)
+        return abs(sum(nums) - sum(digit_iter(nums)))
