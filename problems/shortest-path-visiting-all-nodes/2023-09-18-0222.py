c87ee581e5f2dfe0e8c9d94b70f4765709ced767
Jongsun Suh <jongsun.suh@icloud.com>
2023-09-18 02:22:59 +0000

    Add another accepted version of Shortest Path Visiting All Nodes in Python

---
+++ problems/shortest-path-visiting-all-nodes/2023-09-18-0222.py
@@ -0,0 +1,17 @@
+# Shortest Path Visiting All Nodes
+# https://leetcode.com/problems/shortest-path-visiting-all-nodes/
+# Accepted 2023-09-18 02:22 UTC · Python · 217 ms (43.75%) · 21.7 MB (34.69%)
+
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        n = len(graph)
+        pq = [(0, i, 1 << i) for i in range(n)]
+        visited = set(pq)
+        while pq:
+            steps, u, mask = heappop(pq)
+            if mask == (1 << n) - 1:
+                return steps
+            for v in graph[u]:
+                if (v, mask | (1 << v)) not in visited:
+                    visited.add((v, mask | (1 << v)))
+                    heappush(pq, (steps + 1, v, mask | (1 << v)))
