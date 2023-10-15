b3f94f5d1763125e3ee73e793dc1d61ea428de75
Jongsun Suh <jongsun.suh@icloud.com>
2023-10-15 22:52:09 +0000

    Add another accepted version of Count Unhappy Friends in Python

---
+++ problems/count-unhappy-friends/2023-10-15-2252-2.py
@@ -0,0 +1,21 @@
+# Count Unhappy Friends
+# https://leetcode.com/problems/count-unhappy-friends/
+# Accepted 2023-10-15 22:52 UTC · Python · 359 ms (42.05%) · 29.9 MB (91.79%)
+
+class Solution:
+    def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
+        people = {i: Person(e) for i, e in enumerate(preferences)}
+        pairOf = {**{x: y for x, y in pairs}, **{y: x for x, y in pairs}}
+        return sum(
+            any(x in U.getPreferredOver(v) 
+                for u in X.getPreferredOver(y)
+                    if (U := people[u], v := pairOf[u]))
+            for x in range(n)
+                    if (X := people[x], y := pairOf[x]))
+
+class Person:
+    def __init__(self, preference):
+        self.preference = preference
+    
+    def getPreferredOver(self, this):
+        return set(self.preference[:self.preference.index(this)])
